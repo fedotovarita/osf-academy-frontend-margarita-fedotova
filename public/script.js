c0c82a50714071f0733df8aad475cafc5e51edbb
fedotovarita <rita161@rambler.ru>
2020-11-19 23:48:53 +0200

    Add Featured Products section; made AJAX request

--- public/script.js
+++ public/script.js
@@ -1,9 +1,24 @@
+function asdf(swiperObject) {
+  $.getJSON('featured-items.json', function(data){
+    for(var i = 0; i < data.items.length; i++) {
+      swiperObject.appendSlide(
+        '<div class="swiper-slide featured-slide"><img src="' +
+        data.items[i]['image'] +
+        '" alt=""><span class="featured-prod-description"><p>' +
+        data.items[i]['name'] +
+        '</p><p class="featured-item-label">' +
+        data.items[i]['label'] + 
+        '</p></span></div>'
+      );
+    } 
+  })
+}
+
 $(document).ready(function() {
   $('.menu-burger__header').click(function() {
     $('.menu-burger__header').toggleClass('open-menu');
     $('.main-nav').toggleClass('open-menu');
   });
-
   // $('.nav-list-links').hover(function(){
   //   if (!$('.submenu').hasClass('active')) {
   //     $('.submenu').removeClass('hidden');
@@ -45,11 +60,6 @@ $(document).ready(function() {
     }
   });
 
-  $('#js-carousel').owlCarousel({
-    items: 1,
-    dots: true,
-  });
-
   $('#js-carousel-2').owlCarousel({
     items: 1,
     dots: true,
@@ -66,5 +76,33 @@ $(document).ready(function() {
     loop: true
   });
 
-});
+	var swiper2 = new Swiper('.swiper-container-2', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 4,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  asdf(swiper2);
 
+  $('.more-prod').click(function() {
+    $.getJSON('popular-items.json', function(data){
+      for(var i = 0; i < data.items.length; i++) {
+        $('.popular-item-card').append('<div class="item"><img id="img-prod" src="' +
+        data.items[i]['image'] +
+        '" alt=""><span class="popular-item-card-description"><p id="name-prod">' +
+        data.items[i]['name'] +
+        '</p><p id="price-prod">' +
+        data.items[i]['price'] + '</p></span></div>');
+      }
+  })
+
+  });
+});
